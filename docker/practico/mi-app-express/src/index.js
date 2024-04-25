74b92a270317e8ac81a1d34a50865c44639f7c6e
jgonzalez-clever <julian.gonzalez@cleveritgroup.com>
2024-04-25 15:16:32 -0300

    update practico

--- docker/practico/mi-app-express/src/index.js
+++ docker/practico/mi-app-express/src/index.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const path = require('path'); 
 const routes = require('./routes');
 
 const app = express();
 
-app.use('/', routes);
+// Sirve archivos estáticos desde el directorio 'public'
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
